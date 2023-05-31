a93abc7f59f09c68fdf50ce98ff4d8c8d54560e1
ваше_имя <grigorenko-marko@rambler.ru>
2023-05-31 17:59:53 +0300

    upgraded task01

--- task01.py
+++ task01.py
@@ -7,11 +7,27 @@
 #     3
 #     -> 1
 
+# n=int(input('Введите N: '))
+# a=list(range(1,n+1))
+# print(a)
+# x=int(input('Введите Х: '))
+# if x > 0 and x < n:
+#     print(1)
+# else:
+#     print(0)
+
+
+
 n=int(input('Введите N: '))
-a=list(range(1,n+1))
-print(a)
-x=int(input('Введите Х: '))
-if x > 0 and x < n:
-    print(1)
-else:
-    print(0)
+import random
+rand_list=[]
+for i in range(n):
+    rand_list.append(random.randint(1,n))
+print(*rand_list)
+x=int(input('Введите X: '))
+res=0
+for i in range(len(rand_list)):
+    if rand_list[i]==x:
+        res+=1
+print(res)
+
